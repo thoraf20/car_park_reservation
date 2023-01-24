6d6774beddac1590a433188b8640cbb43e21cde6
Toheeb Rauf <thoraf20@gmail.com>
2023-01-24 08:42:27 +0100

    register and login routes

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -13,7 +13,7 @@ export const registerHandler: RequestHandler = async (req, res) => {
     lastName: Joi.string(),
   })
 
-  const { value, error } = requestSchema.validate(req.params)
+  const { value, error } = requestSchema.validate(req.body)
 
   if (error) {
     return res.status(httpStatus.BAD_REQUEST).json({ error: error.message })
@@ -26,19 +26,26 @@ export const registerHandler: RequestHandler = async (req, res) => {
 
     if (userExist) {
       return res.status(httpStatus.CONFLICT).json({
-        message: 'User data already exist'
+        message: 'User account already exist'
       })
     }
 
     const registerUser = await AuthService.register({
       password: hashPassword(value.password),
-      ...value
+      email: value.email,
+      firstName: value.firstName,
+      lastName: value.lastName
     })
 
-    return res.status(httpStatus.CREATED).json({
-      message: 'User created successfully',
-      data: registerUser
-    })
+    if (registerUser) {
+      return res.status(httpStatus.CREATED).json({
+        message: 'User created successfully',
+      })
+    } else {
+      return res.status(httpStatus.UNPROCESSABLE_ENTITY).json({
+        message: 'Unable to create user',
+      })
+    }
   } catch (error) {
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
       error: error
@@ -52,7 +59,7 @@ export const loginHandler: RequestHandler = async (req, res) => {
     password: Joi.string().required(),
   })
 
-  const { value, error } = requestSchema.validate(req.params)
+  const { value, error } = requestSchema.validate(req.body)
 
   if (error) {
     return res.status(httpStatus.BAD_REQUEST).json({ error: error.message })
@@ -64,8 +71,8 @@ export const loginHandler: RequestHandler = async (req, res) => {
     })
 
     if (!userExist) {
-      return res.status(httpStatus.CONFLICT).json({
-        message: 'User does not exist. Please register ypur account'
+      return res.status(httpStatus.BAD_REQUEST).json({
+        message: 'User account does not exist. Please register your account'
       })
     }
 
@@ -81,7 +88,12 @@ export const loginHandler: RequestHandler = async (req, res) => {
 
     return res.status(httpStatus.OK).json({
       message: 'Login successfully',
-      data: {userExist, token}
+      data: { 
+        firstName: userExist.firstName,
+        lastName: userExist.lastName,
+        email: userExist.email,
+        token 
+      }
     })
   } catch (error) {
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
