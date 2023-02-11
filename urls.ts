d7e29e6b4ae1266bbe676fdcc82947793e89c74e
Toheeb Rauf <thoraf20@gmail.com>
2023-02-11 14:25:29 +0100

    update add garage handler

--- urls.ts
+++ urls.ts
@@ -1,15 +1,13 @@
 import express, { Router } from 'express'
 import { loginHandler, registerHandler } from './src/controller/auth.controller'
-import { createGarageHandler, getGarageHandler } from './src/controller/garage.controller'
+import { addGarageHandler, getGarageHandler } from './src/controller/garage.controller'
 
 const router = Router()
 
 router.post("/register", registerHandler)
 router.post("/login", loginHandler)
 
-router.post("/garage", createGarageHandler)
+router.post("/garage", addGarageHandler)
 router.get("/garage", getGarageHandler)
-
-
 
 export default router
